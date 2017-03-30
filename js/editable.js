7418f65e43d37de31c546c384ee788903d3891f8
fnahuelc <fnahuelc@gmail.com>
2017-03-30 14:51:00 -0300

    agrego data

--- js/editable.js
+++ js/editable.js
@@ -1,8 +1,8 @@
 $(function(){
     $.ajax('admin.html', function (data) {
       var data = $(data).find('h1#adminPromocionExplicacion');
-      console.log(data)
   });
+  console.log(data)
 
     // $("#promocionExplicacion").val(promocionExplicacion);
 
