91896cdb0429956c55e5064115c364c2cd362c3c
fnahuelc <fnahuelc@gmail.com>
2017-03-30 15:18:57 -0300

    agrego data

--- js/editable.js
+++ js/editable.js
@@ -1,6 +1,8 @@
-$.get( "./admin.html #adminPromocionExplicacion", function( data ) {
-
-  console.log('data',data)
+$.get( "./admin.html", function( data ) {
+  info = data.find('#adminPromocionExplicacion')
+  console.log('data',info)
+  mas_info = info.val()
+  console.log('mas info',mas_info)
 });
 
     // $("#promocionExplicacion").val(promocionExplicacion);
