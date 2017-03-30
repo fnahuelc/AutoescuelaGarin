6e9158c7a4071aaedb77e653e4b0c97bd11efbda
fnahuelc <fnahuelc@gmail.com>
2017-03-30 14:58:47 -0300

    agrego data

--- js/editable.js
+++ js/editable.js
@@ -1,5 +1,5 @@
 $(function(){
-    $.ajax('admin.html', function (data) {
+    $.ajax('https://fnahuelc.github.io/AutoescuelaGarin/admin.html', function (data) {
       var data = $(data).find('h1#adminPromocionExplicacion');
       console.log(data)
   });
